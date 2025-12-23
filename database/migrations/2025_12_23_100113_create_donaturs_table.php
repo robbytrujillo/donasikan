3c0a4bf7ad9c2b34dddeb99d173160e7c6469e19
robbytrujillo <robbyikusuma88@gmail.com>
2025-12-23 22:11:58 +0700

    Create Table Database Attributes

--- database/migrations/2025_12_23_100113_create_donaturs_table.php
+++ database/migrations/2025_12_23_100113_create_donaturs_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('donaturs', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('phone_number');
             $table->unsignedBigInteger('fundraising_id');
             $table->unsignedBigInteger('total_amount');
             $table->string('notes');
