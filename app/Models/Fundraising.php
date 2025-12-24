1d2248ce1864256a8c535f5f943914b940e95c98
robbytrujillo <robbyikusuma88@gmail.com>
2025-12-24 16:10:31 +0700

    Setup ORM Relationship For Each Model

--- app/Models/Fundraising.php
+++ app/Models/Fundraising.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Models\Donatur;
 use App\Models\Category;
 use App\Models\Fundraiser;
+use App\Models\FundraisingWithdrawal;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
