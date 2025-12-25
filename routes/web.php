384447894ad27d6a0f297937ae20cdb3c59cc338
robbytrujillo <robbyikusuma88@gmail.com>
2025-12-26 05:57:24 +0700

    Routing Session One

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DonaturController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\FundraiserController;
+use App\Http\Controllers\FundraisingController;
+use App\Http\Controllers\FundraisingPhaseController;
+use App\Http\Controllers\FundraisingWithdrawalController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +21,26 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('admin')->name('admin.')->group(function () {
+
+        Route::resource('categories', CategoryController::class)
+            ->middleware('role:owner');
+
+        Route::resource('donaturs', DonaturController::class)
+            ->middleware('role:owner');
+        Route::resource('fundraisers', FundraiserController::class)
+            ->middleware('role:owner')->except('index');
+
+        Route::resource('fundraising_withdrawals', FundraisingWithdrawalController::class)
+            ->middleware('role:owner|fundraiser');
+
+        Route::resource('fundraising_phases', FundraisingPhaseController::class)
+            ->middleware('role:owner|fundraiser');
+            
+        Route::resource('fundraisings', FundraisingController::class)
+            ->middleware('role:owner|fundraiser');
+    });
 });
 
 require __DIR__.'/auth.php';
