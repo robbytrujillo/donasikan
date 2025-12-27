a3e38d7a170e9cd154e7a1499f96480844e828c9
robbytrujillo <robbyikusuma88@gmail.com>
2025-12-27 17:40:27 +0700

    Create Seeder Role

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $ownerRole = Role::create([
+            'name' => 'owner'
+        ]);
+        
+        $fundraiserRole = Role::create([
+            'name' => 'fundraiser'
+        ]);
+        
+        $userOwner = User::create([
+            'name' => 'Robby Ilhamkusuma',
+            'avatar' => 'image/default-avatar.png',
+            'email' => 'robbyowner@donasikan.com',
+            'password' => bcrypt('qwerty123')
+        ]);
+    }
+}
