7f227365bfd9a1e352bb38cd46eca18872d1f64f
robbytrujillo <robbyikusuma88@gmail.com>
2025-12-31 06:01:50 +0700

    Apply Fundaiser 2

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Fundraiser;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
@@ -11,6 +13,13 @@ class DashboardController extends Controller
     public function apply_fundraiser() {
         $user = Auth::user();
 
+        DB::transaction(function () use ($user) {
+            
+            $validated['user_id'] = $user->id;
+            $validated['is_active'] = false;
+
+            Fundraiser::create($validated);
+        });
         
     }
 }
