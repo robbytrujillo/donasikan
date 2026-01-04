e3940e009f8eaa7d1dbbed450d27280512c08f45
robbytrujillo <robbyikusuma88@gmail.com>
2026-01-05 05:38:29 +0700

    Assets and master layouts 2

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -5,25 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
     @stack('before-style')
-    <link href="{{ asset('css/output.css') }}" rel="stylesheet" />
-    <link
-      rel="icon"
-      type="image/x-icon"
-      href="{{asset('assets/images/icons/donasiku-logo.png')}}"
-    />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap"
-      rel="stylesheet"
-    />
-    <!-- CSS -->
-    <link rel="stylesheet" href="{{asset('assets/js/flickity.min.css')}}" media="screen" />
+      <link href="{{ asset('css/output.css') }}" rel="stylesheet" />
+      <link
+        rel="icon"
+        type="image/x-icon"
+        href="{{asset('assets/images/icons/donasiku-logo.png')}}"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap"
+        rel="stylesheet"
+      />
+      <!-- CSS -->
+      <link rel="stylesheet" href="{{asset('assets/js/flickity.min.css')}}" media="screen" />
     @stack('after-style')
 
   </head>
   <body class="font-poppins text-[#292E4B] bg-[#F6F9FC]">
     
-    {{--  meletakkan konten yang berbeda-beda sesuai route  --}}
+    {{--  mulai meletakkan konten yang berbeda-beda sesuai route  --}}
     @yield('content')
+    {{--  selesai meletakkan konten yang berbeda-beda sesuai route  --}}
 
     @stack('before-script')
         <!-- JavaScript -->
